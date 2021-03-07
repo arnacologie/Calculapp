c5c310431fab4d055f10c68107b01f0a4a8f9f1a
Matthias S <rmanu26e@gmail.com>
2021-03-07 21:50:42 +0100

    First commit

---
+++ app/src/main/java/com/simonmatthias/calculapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.simonmatthias.calculapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
