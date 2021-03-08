589d6fde79fa015c3a23176adca0fdd4fe858358
Matthias S <rmanu26e@gmail.com>
2021-03-08 01:21:35 +0100

    adjust color and textSize

--- app/src/main/java/com/simonmatthias/calculapp/MainActivity.kt
+++ app/src/main/java/com/simonmatthias/calculapp/MainActivity.kt
@@ -2,12 +2,13 @@ package com.simonmatthias.calculapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.fragment_keyboard.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        button_equal.setOnClickListener { v -> println("= clicked!") }
     }
 }
